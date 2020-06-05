feb4a8a7988d536a0f72aa278275a466cc49c258
Shane Vitarana <shanev@gmail.com>
2020-06-04 21:49:45 -0700

    Added reward pool module account (#38)

--- testdata/test_helpers.go
+++ testdata/test_helpers.go
@@ -55,7 +55,7 @@ func CreateTestInput() (*codec.Codec, *SimApp, sdk.Context) {
 	)
 
 	header := abci.Header{Height: app.LastBlockHeight() + 1, Time: time.Now()}
-	app.BeginBlock(abci.RequestBeginBlock{Header: header})
+	// app.BeginBlock(abci.RequestBeginBlock{Header: header})
 	ctx := app.NewContext(false, header)
 
 	return codec.New(), app, ctx
