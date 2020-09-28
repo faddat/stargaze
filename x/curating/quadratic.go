f8d12c27c1e57c84938e1ec3b72aaea557775dbb
Shane Vitarana <shanev@gmail.com>
2020-09-28 12:55:54 -0400

    made QVF data public

--- x/curating/quadratic.go
+++ x/curating/quadratic.go
@@ -9,9 +9,9 @@ import (
 type QVFData struct {
 	ctx        sdk.Context
 	keeper     keeper.Keeper
-	voterCount int64
-	votingPool sdk.Int
-	rootSum    sdk.Dec
+	VoterCount int64
+	VotingPool sdk.Int
+	RootSum    sdk.Dec
 }
 
 // NewQVFData returns an instance of QVFData
@@ -19,28 +19,28 @@ func NewQVFData(ctx sdk.Context, k keeper.Keeper) QVFData {
 	return QVFData{
 		ctx:        ctx,
 		keeper:     k,
-		votingPool: sdk.ZeroInt(),
-		rootSum:    sdk.ZeroDec(),
+		VotingPool: sdk.ZeroInt(),
+		RootSum:    sdk.ZeroDec(),
 	}
 }
 
 // TallyVote tallies a vote
 func (q QVFData) TallyVote(amount sdk.Int) (QVFData, error) {
-	q.voterCount++
-	q.votingPool = q.votingPool.Add(amount)
+	q.VoterCount++
+	q.VotingPool = q.VotingPool.Add(amount)
 
 	sqrt, err := amount.ToDec().ApproxSqrt()
 	if err != nil {
 		return q, err
 	}
-	q.rootSum = q.rootSum.Add(sqrt)
+	q.RootSum = q.RootSum.Add(sqrt)
 
 	return q, nil
 }
 
 // MatchPool calculates and returns the quadratic match pool
 func (q QVFData) MatchPool() sdk.Dec {
-	idealPoolSize := q.rootSum.Power(2).Sub(q.votingPool.ToDec())
+	idealPoolSize := q.RootSum.Power(2).Sub(q.VotingPool.ToDec())
 
 	rewardPool := q.keeper.GetRewardPoolBalance(q.ctx).Amount.ToDec()
 	maxPoolPercent := q.keeper.GetParams(q.ctx).RewardPoolCurationMaxAlloc
@@ -55,16 +55,16 @@ func (q QVFData) MatchPool() sdk.Dec {
 
 // VoterReward returns the distribution for a voter
 func (q QVFData) VoterReward() sdk.Int {
-	if q.voterCount == 0 {
+	if q.VoterCount == 0 {
 		return sdk.ZeroInt()
 	}
-	return q.votingPool.QuoRaw(q.voterCount)
+	return q.VotingPool.QuoRaw(q.VoterCount)
 }
 
 // MatchReward returns the funding match for a voter
 func (q QVFData) MatchReward() sdk.Dec {
-	if q.voterCount == 0 {
+	if q.VoterCount == 0 {
 		return sdk.ZeroDec()
 	}
-	return q.MatchPool().QuoInt64(q.voterCount)
+	return q.MatchPool().QuoInt64(q.VoterCount)
 }
